5ce24d5059b6b0b63dd3d55732270624b5d30840
Aaron Gertler <aaron.gertler@gmail.com>
2020-11-07 18:09:48 -0600

    cleaned up code

--- src/index.js
+++ src/index.js
@@ -41,23 +41,26 @@ class Board extends React.Component {
         return (
             <div>
                 <div className="status">{status}</div>
+
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
                 </div>
+
                 <div className="board-row">
                     {this.renderSquare(3)}
                     {this.renderSquare(4)}
                     {this.renderSquare(5)}
                 </div>
+
                 <div className="board-row">
                     {this.renderSquare(6)}
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
                 </div>
             </div>
-        );
+        )
     }
 }
 
@@ -68,12 +71,13 @@ class Game extends React.Component {
                 <div className="game-board">
                     <Board />
                 </div>
+
                 <div className="game-info">
                     <div>{/* status */}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
-        );
+        )
     }
 }
 
@@ -82,7 +86,7 @@ class Game extends React.Component {
 ReactDOM.render(
     <Game />,
     document.getElementById('root')
-);
+)
 
 function calculateWinner(squares) {
     const lines = [
@@ -94,12 +98,14 @@ function calculateWinner(squares) {
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6],
-    ];
+    ]
+
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
             return squares[a];
         }
     }
+
     return null;
 }
