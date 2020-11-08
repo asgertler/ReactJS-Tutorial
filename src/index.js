a9d4e674d2487a6ee3ea1006a3a075b68017da6b
Aaron Gertler <aaron.gertler@gmail.com>
2020-11-07 23:57:58 -0600

    added move list

--- src/index.js
+++ src/index.js
@@ -79,6 +79,19 @@ class Game extends React.Component {
         const current = history[history.length - 1]
         const winner = calculateWinner(current.squares)
 
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                "Go to move #" + move :
+                "Go to game start"
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>
+                        {desc}
+                    </button>
+                </li>
+            )
+        })
+
         let status
 
         if (winner) {
@@ -98,7 +111,7 @@ class Game extends React.Component {
 
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         )
